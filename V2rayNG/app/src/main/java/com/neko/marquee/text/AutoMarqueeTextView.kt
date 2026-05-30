1dbfe15c0f053f09d20da015775c95520585fc7c
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-05-30 23:54:36 +0700

    improvement again

--- V2rayNG/app/src/main/java/com/neko/marquee/text/AutoMarqueeTextView.kt
+++ V2rayNG/app/src/main/java/com/neko/marquee/text/AutoMarqueeTextView.kt
@@ -3,58 +3,36 @@ package com.neko.marquee.text
 import android.content.Context
 import android.text.TextUtils
 import android.util.AttributeSet
-import android.view.View
 import androidx.appcompat.widget.AppCompatTextView
 
-class AutoMarqueeTextView : AppCompatTextView {
-    private var mAggregatedVisible: Boolean = false
-    private var mMarqueePosition: Int = 0
+class AutoMarqueeTextView @JvmOverloads constructor(
+    context: Context, 
+    attrs: AttributeSet? = null, 
+    defStyleAttr: Int = android.R.attr.textViewStyle
+) : AppCompatTextView(context, attrs, defStyleAttr) {
 
-    constructor(context: Context) : super(context) {
-        mAggregatedVisible = false
-    }
-
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        mAggregatedVisible = false
-    }
-
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        mAggregatedVisible = false
+    init {
+        isSingleLine = true
+        ellipsize = TextUtils.TruncateAt.MARQUEE
+        marqueeRepeatLimit = -1
+        setHorizontallyScrolling(true)
+        isHorizontalFadingEdgeEnabled = false 
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        isSelected = true
+        isSelected = true 
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        isSelected = false
+        isSelected = false 
     }
 
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        onVisibilityAggregated(isVisibleToUser())
-    }
-
-    override fun onVisibilityAggregated(isVisible: Boolean) {
-        super.onVisibilityAggregated(isVisible)
-        if (isVisible == mAggregatedVisible) return
-        mAggregatedVisible = isVisible
-        if (mAggregatedVisible) {
-            ellipsize = TextUtils.TruncateAt.MARQUEE
-            post {
-                scrollTo(mMarqueePosition, 0)
-                isSelected = true
-            }
-        } else {
-            mMarqueePosition = scrollX
-            ellipsize = TextUtils.TruncateAt.END
-            isSelected = false
+    override fun onWindowFocusChanged(hasWindowFocus: Boolean) {
+        super.onWindowFocusChanged(hasWindowFocus)
+        if (hasWindowFocus) {
+            isSelected = true
         }
-    }
-
-    fun View.isVisibleToUser(): Boolean {
-        return this.visibility == View.VISIBLE
     }
 }
