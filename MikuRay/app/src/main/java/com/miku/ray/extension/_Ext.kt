f73583cf1d7afe3a239cf2e5da07fbc03bbfafce
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-08-29 18:57:11 +0700

    fix: URL test progress event decoding

--- MikuRay/app/src/main/java/com/miku/ray/extension/_Ext.kt
+++ MikuRay/app/src/main/java/com/miku/ray/extension/_Ext.kt
@@ -399,14 +399,24 @@ fun Context.listenForPackageChanges(onetime: Boolean = true, callback: () -> Uni
         }
     }
 
-inline fun <reified T : Serializable> Bundle.serializable(key: String): T? = when {
-    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializable(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getSerializable(key) as? T
+inline fun <reified T : Serializable> Bundle.serializable(key: String): T? {
+    classLoader = T::class.java.classLoader
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getSerializable(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getSerializable(key) as? T
+    }
 }
 
-inline fun <reified T : Serializable> Intent.serializable(key: String): T? = when {
-    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getSerializableExtra(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getSerializableExtra(key) as? T
+inline fun <reified T : Serializable> Intent.serializable(key: String): T? {
+    setExtrasClassLoader(T::class.java.classLoader)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getSerializableExtra(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getSerializableExtra(key) as? T
+    }
 }
 
 fun CharSequence?.isNotNullEmpty(): Boolean = !this.isNullOrBlank()
